3cd0e9d9d77766287030b60a188169b9f8fabe4e
unknown <temmietem16@gmail.com>
2026-06-16 18:38:38 +0800

    for env

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jkfdoeoplrtijbursfcd.supabase.co';
-const supabaseKey = 'sb_publishable_6jCBgulUby93nS1gvFJ5lg_wepCuShC';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
